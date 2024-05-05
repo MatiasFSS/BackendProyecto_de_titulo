1a409cdefb83e76a98a157be4595778c2093aa70
Matias Sanchez <matias.fss97@gmail.com>
2024-05-05 11:14:27 -0400

    deploy73

--- src/DB/mysql.js
+++ src/DB/mysql.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const config = require('../config');
 //const { error } = require('../red/respuesta');
 //const { Pregunta } = require('../modulos/Trivias');
--- src/config.js
+++ src/config.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 
 module.exports = {
     app:{
-        port: process.env.PORT || 3306
+        port: process.env.PORT || 5000
     }, 
     mysql: {
         host: process.env.MYSQL_HOST || 'sql.freedb.tech',
         user: process.env.MYSQL_USER || 'freedb_matias',
-        password: process.env.MYSQL_PASSWORD || "J%J%fVKs37XqZpf87",
-        database: process.env.MYSQL_DB || "freedb_proyecto_titulo",
+        password: process.env.MYSQL_PASSWORD || 'J%J%fVKs37XqZpf87',
+        database: process.env.MYSQL_DB || 'freedb_proyecto_titulo',
     
     }
 }
